c293acb9bb2157d02b11be31aad1e38264c444ec
martinperezmaneiro <martinperezmaneiro@gmail.com>
2022-03-03 11:57:51 +0100

    Restructured statistics prints

--- scripts/get_stat_info.py
+++ scripts/get_stat_info.py
@@ -11,16 +11,29 @@ stat_df = dio.load_dst(file, 'stat', 'stat')
 
 nevent = sum(stat_df.nevent)
 signal_nevent = sum(stat_df.signal_nevent)
+fail_nevent = sum(stat_df.fail_nevent)
 nevent_bkg_lower_fail = sum(stat_df.nevent_bkg_lower_fail)
 nevent_bkg_upper_fail = sum(stat_df.nevent_bkg_upper_fail)
 nevent_sig_lower_fail = sum(stat_df.nevent_sig_lower_fail)
 nevent_sig_upper_fail = sum(stat_df.nevent_sig_upper_fail)
-total_fails = nevent_bkg_lower_fail + nevent_bkg_upper_fail + nevent_sig_lower_fail + nevent_sig_upper_fail
 
-print('Out of {nevent} events, the {percentage:.3f}% were double scape'.format(nevent = nevent, percentage = signal_nevent * 100 / nevent))
+print('Out of {nevent} events, the {percentage:.3f}% ({number} events) were double scape'.format(nevent = nevent,
+                                                                                                 percentage = signal_nevent * 100 / nevent,
+                                                                                                 number = signal_nevent))
 print('####')
-print('Out of {nevent} events, the {percentage:.3f}% did not match binclass with the counted blobs'.format(nevent = nevent, percentage = total_fails * 100 / nevent))
-print('Out of {nevent} events, the {percentage:.3f}% were from the bkg with less than 1 blob'.format(nevent = nevent, percentage = nevent_bkg_lower_fail * 100 / nevent))
-print('Out of {nevent} events, the {percentage:.3f}% were from the bkg with more than 1 blob'.format(nevent = nevent, percentage = nevent_bkg_upper_fail * 100 / nevent))
-print('Out of {nevent} events, the {percentage:.3f}% were from the dsc with less than 2 blob'.format(nevent = nevent, percentage = nevent_sig_lower_fail * 100 / nevent))
-print('Out of {nevent} events, the {percentage:.3f}% were from the dsc with more than 2 blob'.format(nevent = nevent, percentage = nevent_sig_upper_fail * 100 / nevent))
+print('Labelling Statistics')
+print('Out of {nevent} events, the {percentage:.3f}% ({number} events) did not match binclass with the counted blobs'.format(nevent = nevent,
+                                                                                                                             percentage = fail_nevent * 100 / nevent,
+                                                                                                                             number = fail_nevent))
+print('-->Out of {nevent} events, the {percentage:.3f}% ({number} events) were from the bkg with less than 1 blob'.format(nevent = nevent,
+                                                                                                                       percentage = nevent_bkg_lower_fail * 100 / nevent,
+                                                                                                                       number = nevent_bkg_lower_fail))
+print('-->Out of {nevent} events, the {percentage:.3f}% ({number} events) were from the bkg with more than 1 blob'.format(nevent = nevent,
+                                                                                                                       percentage = nevent_bkg_upper_fail * 100 / nevent,
+                                                                                                                       number = nevent_bkg_upper_fail))
+print('-->Out of {nevent} events, the {percentage:.3f}% ({number} events) were from the dsc with less than 2 blob'.format(nevent = nevent,
+                                                                                                                       percentage = nevent_sig_lower_fail * 100 / nevent,
+                                                                                                                       number = nevent_sig_lower_fail))
+print('-->Out of {nevent} events, the {percentage:.3f}% ({number} events) were from the dsc with more than 2 blob'.format(nevent = nevent,
+                                                                                                                       percentage = nevent_sig_upper_fail * 100 / nevent,
+                                                                                                                       number = nevent_sig_upper_fail))
