a327d1fc43565eb1e7a4413416065a1cbdf67446
Martin <martinperezmaneiro@gmail.com>
2025-03-14 13:01:07 +0100

    Generalize setup file

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-export DLDIR=$HOME/data_labelling/
+export DLDIR=$( cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 ; pwd -P )/
 export PYTHONPATH=$DLDIR:$PYTHONPATH
 export PATH=$DLDIR/bin:$PATH
 export PATH=$DLDIR/scripts:$PATH
